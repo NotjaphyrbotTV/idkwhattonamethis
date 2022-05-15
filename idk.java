06c06adc54b2bb21c6d72ef46dc2f7fb4cd8013c
NotjaphyrbotTV <92495089+NotjaphyrbotTV@users.noreply.github.com>
2022-05-15 17:02:29 -0500

    idkwhattonamethis

---
+++ idk.java
@@ -0,0 +1,3 @@
+/**if your reading this AHH! https://www.meridianoutpost.com/resources/etools/calculators/morsecode/morsecode_37fbd92625eda0783d87215c1fafc17c.wav/**
+ /**if you figure the last one i...https://www.meridianoutpost.com/resources/etools/calculators/morsecode/morsecode_37fbd92625eda0783d87215c1fafc17c.wav/**
+ /**im darkip... OH NO I DONT HAVE TIME BYE LEARN WITH THIS LINK:https://www.meridianoutpost.com/resources/etools/calculators/morsecode/morsecode_37fbd92625eda0783d87215c1fafc17c.wav/**
